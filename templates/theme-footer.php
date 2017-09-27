1a628c26c7b892c8aa7d088e1bb597744ab8c60c
Saurabh Shukla <saurabh@yapapaya.com>
2017-09-27 21:31:36 +0530

    Fixes credit text

--- templates/theme-footer.php
+++ templates/theme-footer.php
@@ -5,7 +5,7 @@
  * @author Saurabh Shukla <contact.saurabhshukla@gmail.com>
  */
 ?>
-<p class="wpin-dc-theme-footer"><?php _e( 'Hosting sponsored by', 'wpin-dc' ); ?>
+<p class="wpin-dc-theme-footer"><?php _e( 'Hosting Sponsored by', 'wpin-dc' ); ?>
 	<a href="https://bluehost.in" title="<?php _e( 'BlueHost India', 'wpin-dc' ); ?>">
 		<img src="<?php echo WPIN_DC_URL . 'assets/images/bluehost-in-logo.png' ?>" alt="<?php _e( 'BlueHost India', 'wpin-dc' ); ?>">
 	</a>
