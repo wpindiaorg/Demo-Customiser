ad79f8d1e522ea4939021719028a58ac34b1b03b
Saurabh Shukla <saurabh@yapapaya.com>
2017-09-27 22:28:11 +0530

    Removes unnecessary lines

--- templates/credits.php
+++ templates/credits.php
@@ -1,6 +1,4 @@
-
 <?php _e( 'Hosting Sponsored by', 'wpin-dc' ); ?>
 <a href="https://bluehost.in" title="<?php _e( 'BlueHost India', 'wpin-dc' ); ?>">
 	<img style="vertical-align:middle;" src="<?php echo WPIN_DC_URL . 'assets/images/bluehost-in-logo.png' ?>" alt="<?php _e( 'BlueHost India', 'wpin-dc' ); ?>">
 </a>
-
