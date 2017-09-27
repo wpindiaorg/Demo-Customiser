b0fe6a3cdde913304d374be8b5e5bef27a6d8e31
Saurabh Shukla <saurabh@yapapaya.com>
2017-09-27 19:33:10 +0530

    Initialses repository

---
+++ demo-customiser.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+  Plugin Name: Demo Customiser
+  Plugin URI: 
+  Description: MU (must-use) plugin to customise demo sites for Indian languages on demo.wpindia.org
+  Version: 0.1
+  Author: saurabhshukla,
+  Author URI: http://github.com/yapapaya/
+  Text Domain: wpindia-demo
+  Domain Path: /languages
+  License: GNU General Public License v2 or later
+ */
+
+/*
+ * Features:
+ * 1. Filter footer to add sponsor credits
+ * 2. Add a default post for different languages
+ * 3. (Maybe) add a default comment on this post 
+ * 
+ * others may be added later
+ */
