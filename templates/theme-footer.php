68e0ef9c6680d1bc3cea243d5040f5ac71f20068
Saurabh Shukla <saurabh@yapapaya.com>
2017-09-27 19:53:57 +0530

    Adds template for theme footer credits

---
+++ templates/theme-footer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Partial to add sponsor credits to theme footer
+ * 
+ * @author Saurabh Shukla <contact.saurabhshukla@gmail.com>
+ */
+?>
+<div class="wpin-dc-theme-footer">
+	<p>Hosting Sponsored by <a href="https://bluehost.in" alt="Bluehost India">Bluehost India</a></p> 
+</div>
+
